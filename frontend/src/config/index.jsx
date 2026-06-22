7cb4ad893f52c0f27c29356ca5d2fc2d6fc9e001
arun-singh-codes <arun.singh.codes@gmail.com>
2026-06-22 08:19:35 +0530

    last

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -4,3 +4,5 @@ export const clientServer = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
     withCredentials: true
 });
+
+console.log("API URL =", process.env.NEXT_PUBLIC_API_URL);
